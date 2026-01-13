b3c28e58329a672e1e2a11a90e6967a77a9be929
fravila08 <fr4v1l4@gmail.com>
2026-01-13 07:30:31 -0800

    ready for lesson

---
+++ cypress/e2e/home.cy.jsx
@@ -0,0 +1,8 @@
+describe('PokemonCard App Home Page', () => {
+
+  it('displays Pikachu on initial load', () => {
+    cy.visit('/')
+    cy.get('#welcome').should('have.text', 'Vite + React (CI/CD)')
+  });
+
+});
