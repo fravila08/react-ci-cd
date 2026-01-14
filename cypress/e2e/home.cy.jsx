c870d57977820f84756a7ebb167342d378be6e52
fravila08 <fr4v1l4@gmail.com>
2026-01-14 07:53:11 -0800

    update test for h1 element

--- cypress/e2e/home.cy.jsx
+++ cypress/e2e/home.cy.jsx
@@ -2,7 +2,7 @@ describe('PokemonCard App Home Page', () => {
 
   it('displays Pikachu on initial load', () => {
     cy.visit('/')
-    cy.get('#welcome').should('have.text', 'Vite + React (CI/CD)')
+    cy.get('#welcome').should('have.text', 'Vite + React (CI/CD) Completed')
   });
 
 });
